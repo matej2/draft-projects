43da870822ca84105a753580aa9518c9cea53ae5
Matej2 <11059438+matej2@users.noreply.github.com>
2026-05-20 14:37:22 +0200

    Initial commit - project example

---
+++ fast-api/storage.py
@@ -0,0 +1,28 @@
+import json
+import logging
+from pathlib import Path
+
+
+from models import ProjectCreate, ProjectOut
+
+
+class Storage:
+    def __init__(self):
+        self.DATA_DIR = Path("data")
+        self.DATA_FILE = self.DATA_DIR / "projects.json"
+        self.logger = logging.getLogger(__name__)
+
+    def load_data(self) -> list[ProjectOut]:
+        if self.DATA_FILE.is_file() and self.DATA_FILE.exists():
+            with open(self.DATA_FILE, "r") as f:
+                content = f.read()
+                return json.loads(content)
+        else:
+            return list()
+
+    def save_data(self, data: list[ProjectCreate]):
+        self.DATA_DIR.mkdir(parents=True, exist_ok=True)
+        with open(self.DATA_FILE, "w") as f:
+            json_text = json.dumps(data, indent=2)
+            #self.logger.info(f"Saving data to {self.DATA_FILE}: {json_text}")
+            f.write(json_text)
