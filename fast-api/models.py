8699174a3a478bcf2d989bf5625cb7bf73241bbb
Matej2 <11059438+matej2@users.noreply.github.com>
2026-05-21 10:23:14 +0200

    Custom base model

--- fast-api/models.py
+++ fast-api/models.py
@@ -1,30 +1,42 @@
+from decimal import Decimal
 from enum import Enum
 from typing import Optional
 
+from pip._internal.utils import datetime
 from pydantic import BaseModel, Field
+from pydantic.alias_generators import to_camel
+
+
+class CustomBase(BaseModel):
+    class Config:
+        alias_generator = to_camel
+        populate_by_name = True
+        json_encoders = {
+            Decimal: str
+        }
 
 
 class ProjectStatus(str, Enum):
     active = "active"
     inactive = "inactive"
 
-class ProjectDomain(BaseModel):
+class ProjectDomain(CustomBase):
     id: str
     name: str
     description: str
     status: ProjectStatus
 
-class ProjectCreate(BaseModel):
+class ProjectCreate(CustomBase):
     name: str = Field(min_length=1,max_length=100)
     description: str = Field(min_length=1, max_length=255)
     status: ProjectStatus = ProjectStatus.inactive
 
-class ProjectUpdate(BaseModel):
+class ProjectUpdate(CustomBase):
     name: Optional[str] = Field(min_length=1,max_length=100)
     description: Optional[str] = Field(min_length=1, max_length=255)
     status: Optional[ProjectStatus] = ProjectStatus.active
 
-class ProjectOut(BaseModel):
+class ProjectOut(CustomBase):
     name: str
     description: str
     status : ProjectStatus
