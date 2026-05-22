94e364045cb05afd6ada9ea24aaa81f8dc6e3da1
Matej2 <11059438+matej2@users.noreply.github.com>
2026-05-22 14:33:58 +0200

    Image upload

--- image-sharing/app/app.py
+++ image-sharing/app/app.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+import tempfile
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, HTTPException, UploadFile, File, Form
@@ -6,6 +9,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db import create_db_and_tables, get_async_session, Post
+from app.image import imagekit
 from app.schemas import PostCreate
 
 @asynccontextmanager
@@ -15,23 +19,43 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-
-
 @app.post("/upload")
 async def upload_file(
     file: UploadFile = File(...),
     caption: str = Form(""),
     session: AsyncSession = Depends(get_async_session)
 ):
-    post = Post(
-        caption=caption,
-        url= file.filename,
-        file_type=file.content_type,
-        file_name=file.filename
-    )
 
-    session.add(post);
-    await session.commit()
+    temp_file = None
+
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)
+
+        upload_result = imagekit.files.upload(
+            file=open(temp_file_path, "rb"),
+            file_name=file.filename,
+            use_unique_file_name=True,
+            tags=["backend-upload"]
+        )
+        if upload_result is not None:
+            post = Post(
+                caption=caption,
+                url=upload_result.url,
+                file_type="video" if file.content_type.startswith("video/") else "image",
+                file_name=upload_result.name
+            )
+            session.add(post)
+            await session.commit()
+            await session.refresh(post)
+            return post
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        file.file.close()
 
 @app.get("/feed")
 async def get_feed(session: AsyncSession = Depends(get_async_session)):
---
+++ image-sharing/app/image.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+from imagekitio import ImageKit
+
+load_dotenv()
+imagekit = ImageKit(
+    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY")
+)
