a88637d2873138a8d86bd35fbae7f867f75cffde
Matej2 <11059438+matej2@users.noreply.github.com>
2026-06-18 09:41:10 +0200

    Code refactor

--- GpxClient/main.py
+++ GpxClient/main.py
@@ -9,19 +9,20 @@ from model.PointInput import PhotoInput
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-app = FastAPI(title="OneDrive API")
+app = FastAPI(title="GPX Client")
 
 @app.post("/")
-async def login(file: UploadFile = File(...)):
+async def process_gpx(file: UploadFile = File(...)):
     contents = await file.read()
     gpx = gpxpy.parse(contents)
 
     for track in gpx.tracks:
         for segment in track.segments:
-            print([
+            return ([
                 Point(point.latitude, point.longitude, point.elevation)
                 for point in segment.points
             ])
+    return ()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
