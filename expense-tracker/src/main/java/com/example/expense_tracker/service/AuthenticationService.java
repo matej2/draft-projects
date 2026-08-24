48c94e270984f8dfddcee7ce5eff72db20a665b1
Matej2 <11059438+matej2@users.noreply.github.com>
2026-08-24 16:27:41 +0200

    Added save user token and revoke user token,

--- expense-tracker/src/main/java/com/example/expense_tracker/service/AuthenticationService.java
+++ expense-tracker/src/main/java/com/example/expense_tracker/service/AuthenticationService.java
@@ -5,7 +5,10 @@ import com.example.expense_tracker.domain.dto.AuthenticationRequest;
 import com.example.expense_tracker.domain.dto.AuthenticationResponse;
 import com.example.expense_tracker.domain.dto.RegisterRequest;
 import com.example.expense_tracker.domain.entity.Role;
+import com.example.expense_tracker.domain.entity.Token;
+import com.example.expense_tracker.domain.entity.TokenType;
 import com.example.expense_tracker.domain.entity.User;
+import com.example.expense_tracker.repository.TokenRepository;
 import com.example.expense_tracker.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,6 +23,7 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final TokenRepository tokenRepository;
 
     public AuthenticationResponse register(RegisterRequest request) {
         User user = User.builder()
@@ -47,7 +51,32 @@ public class AuthenticationService {
         );
         User user = userRepository.findByEmail(request.getEmail()).orElseThrow();
         String jwtToken = jwtService.generateToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken).build();
     }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .registeredUser(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
 }
