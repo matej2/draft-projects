452741d75734b4203d898a6c8b0a25fe5312b25a
Matej2 <11059438+matej2@users.noreply.github.com>
2026-09-16 09:37:08 +0200

    Entities and migrations for budget and savings goal

---
+++ expense-tracker/src/main/resources/db/migration/02-create-budget.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+
+
+    -- TABLES
+
+    CREATE TABLE IF NOT EXISTS budget (
+      id serial primary key,
+      monthly_limit real,
+      month smallint,
+      category integer references category(id)
+    );
+
+    -- Permission grants
+    GRANT SELECT, INSERT, UPDATE, DELETE ON budget TO app_user;
+
+
+EOSQL
---
+++ expense-tracker/src/main/resources/db/migration/03-create-savings-goal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+
+
+    -- TABLES
+
+    CREATE TABLE IF NOT EXISTS savingsgoal (
+      id serial primary key,
+      name varchar(100),
+      target_amount real,
+      current_amount real,
+      deadline date
+    );
+
+    -- Permission grants
+    GRANT SELECT, INSERT, UPDATE, DELETE ON savingsgoal TO app_user;
+
+
+EOSQL
