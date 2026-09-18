8785130436b4fd9427fe0d873780c76416ecc66e
Matej2 <11059438+matej2@users.noreply.github.com>
2026-09-18 14:53:51 +0200

    Refactored web controller

--- expense-tracker/src/main/java/com/example/expense_tracker/controller/WebController.java
+++ expense-tracker/src/main/java/com/example/expense_tracker/controller/WebController.java
@@ -1,13 +1,13 @@
 package com.example.expense_tracker.controller;
 
-import com.example.expense_tracker.domain.dto.CategoryResponse;
 import com.example.expense_tracker.domain.dto.ExpenseRequest;
 import com.example.expense_tracker.domain.dto.ExpenseResponse;
-import com.example.expense_tracker.domain.dto.FrequencyResponse;
 import com.example.expense_tracker.service.CategoryService;
 import com.example.expense_tracker.service.ExpenseTrackingService;
 import com.example.expense_tracker.service.FrequencyService;
+import com.example.expense_tracker.service.InsightService;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -27,37 +27,43 @@ public class WebController {
     private final ExpenseTrackingService expenseTrackingService;
     private final FrequencyService frequencyService;
     private final CategoryService categoryService;
+    private final InsightService insightService;
 
-    @GetMapping
-    public String home(Model model, Integer pageNumber) {
-        pageNumber = pageNumber == null ? 0 : pageNumber;
-        Pageable sortedByDate =
-                PageRequest.of(pageNumber, 40, Sort.by("expenseDate").descending());
-
-        List<ExpenseResponse> expenses = expenseTrackingService.getExpense(sortedByDate);
-        List<FrequencyResponse> frequencies = frequencyService.getFrequency();
-        List<CategoryResponse> categories = categoryService.getAllCategories();
-
-        model.addAttribute("pageNumber", pageNumber);
-
-        Map<String, List<? extends Record>> attributes = Map.of(
-                "expenses", expenses,
-                "frequencies", frequencies,
-                "categories", categories
-        );
-
-        model.addAllAttributes(attributes);
-
+    private boolean isAuthenticated() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Boolean isAuthenticated = true;
 
         if (authentication instanceof AnonymousAuthenticationToken) {
             isAuthenticated = false;
         }
-        model.addAttribute("isAuthenticated", isAuthenticated);
+        return isAuthenticated;
+    }
 
-        model.addAttribute("expenseRequest",new ExpenseRequest(null, null, null, null, null, null));
+    @GetMapping
+    public String home(Model model, Integer pageNumber) {
+        pageNumber = pageNumber == null ? 0 : pageNumber;
+        Pageable sortedByDate = PageRequest.of(
+                pageNumber,
+                40,
+                Sort.by("expenseDate").descending());
+        List<ExpenseResponse> expenses = expenseTrackingService.getExpense(sortedByDate);
+
+        Map<String, Object> attributes = getAttributesForExpenses(pageNumber, expenses);
+        model.addAllAttributes(attributes);
+
         return "index";
+    }
+
+    private @NonNull Map<String, Object> getAttributesForExpenses(Integer pageNumber, List<ExpenseResponse> expenses) {
+        Map<String, Object> attributes = Map.of(
+                "expenses", expenses,
+                "frequencies", frequencyService.getFrequency(),
+                "categories", categoryService.getAllCategories(),
+                "pageNumber", pageNumber,
+                "isAuthenticated", isAuthenticated(),
+                "expenseRequest",new ExpenseRequest(null, null, null, null, null, null)
+        );
+        return attributes;
     }
 
     @PostMapping("/submitExpense")
